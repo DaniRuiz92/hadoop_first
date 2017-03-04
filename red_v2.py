e4aae05335b1e8edc4e6a4d782f442f5679167b7
DaniRuiz92 <danielruizriquelme@gmail.com>
2017-03-04 20:23:56 +0100

    Update

--- red_v2.py
+++ red_v2.py
@@ -13,12 +13,12 @@ for input_line in sys.stdin:
    if last_key == this_key:
        running_total += value
    else:
-       if last_key:
-           if sys.argv[1]<running_total:
-               print( "%s\t%d" % (last_key, running_total) )
+       if last_key & running_total > sys.argv:
+           print( "%s\t%d" % (last_key, running_total) )
        running_total = value
        last_key = this_key
  
-if last_key == this_key:
-    if sys.argv[1]<running_total:
-        print( "%s\t%d" % (last_key, running_total) )
+if last_key == this_key & running_total > sys.argv:
+    print( "%s\t%d" % (last_key, running_total) )
+
+#if sys.argv[1]<running_total:
