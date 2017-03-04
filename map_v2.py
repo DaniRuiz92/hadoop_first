af5fde4cb5577fd7269e9f0f23f0f7eae9149b84
DaniRuiz92 <danielruizriquelme@gmail.com>
2017-03-04 22:39:44 +0100

    Update

--- map_v2.py
+++ map_v2.py
@@ -5,6 +5,6 @@ import sys
 for line in sys.stdin:
     line = line.strip()
     keys = line.split()
-    for idx, key in range(0,len(keys)-1):
+    for idx in range(0,len(keys)-1):
         value = 1
         print( "%s\t%d" % (keys[idx]+'-'+keys[idx+1], value) )
