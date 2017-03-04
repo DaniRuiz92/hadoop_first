b7ff9fb1da29e66db05e1e3b0f544fdb1e9298d4
DaniRuiz92 <danielruizriquelme@gmail.com>
2017-03-04 22:37:14 +0100

    Mapper for 2 words

---
+++ map_v2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    keys = line.split()
+    for idx, key in range(0,len(keys)-1):
+        value = 1
+        print( "%s\t%d" % (keys[idx]+'-'+keys[idx+1], value) )
